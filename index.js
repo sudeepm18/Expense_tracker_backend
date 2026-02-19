2e72bad9a7b37087e13f56bde7af11fd1c77be54
Sudeep <sudeepmorabad21@gmail.com>
2026-02-19 16:55:13 +0530

    fixing

--- index.js
+++ index.js
@@ -40,5 +40,9 @@ app.get("/api/protected", authMiddleware, (req, res) => {
 })
 
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server running on ${PORT}`))
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
